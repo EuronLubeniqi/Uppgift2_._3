22316e5f7abe531c03c2c2d3197de2ffae49e2f5
EuronLubeniqi <eulu07001@utb.vaxjo.se>
2024-10-03 14:18:24 +0200

    Add project files.

---
+++ Uppgift2_._3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Uppgift2_._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många dagar ska du använda bilen");
+            int dagar = int.Parse(Console.ReadLine());
+            int Dag1 = 300;
+            int Övrigadagar = 500;
+            Console.WriteLine("Hur många kilometer ska du köra bilen?");
+            int kilometer = int.Parse(Console.ReadLine());  
+            int sum = Dag1 + Övrigadagar + kilometer;
+            Console.WriteLine(sum);
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
